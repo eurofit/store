64428b5fc98aaf105120726557fcf275a76bdd2d
abdirazak <eurofitltd@gmail.com>
2026-01-03 15:44:48 +0300

    adds favicon metadata

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,32 +1,35 @@
-import { dmSans } from '@/app/fonts/dm-sans';
-import { montserrat } from '@/app/fonts/montserrat';
-import '@/app/globals.css';
-import { TailwindIndicator } from '@/components/tailwind-indicator';
-import { Toaster } from '@/components/ui/sonner';
-import { site } from '@/constants/site';
-import { JotaiProvider } from '@/providers/jotai';
-import { ReactQueryProvider } from '@/providers/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { Metadata, Viewport } from 'next';
-import NextTopLoader from 'nextjs-toploader';
+import { dmSans } from "@/app/fonts/dm-sans"
+import { montserrat } from "@/app/fonts/montserrat"
+import "@/app/globals.css"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { Toaster } from "@/components/ui/sonner"
+import { site } from "@/constants/site"
+import { JotaiProvider } from "@/providers/jotai"
+import { ReactQueryProvider } from "@/providers/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import type { Metadata, Viewport } from "next"
+import NextTopLoader from "nextjs-toploader"
 
 export const metadata: Metadata = {
-  metadataBase: new URL(site.url!),
+  metadataBase: new URL(site.url),
   title: {
     default: site.name,
     template: `%s - ${site.name}`,
   },
   description: site.description,
-};
+  other: {
+    "apple-mobile-web-app-title": site.name,
+  },
+}
 
 export const viewport: Viewport = {
-  themeColor: '#fb2c36',
-};
+  themeColor: "#fb2c36",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -52,5 +55,5 @@ export default function RootLayout({
         </JotaiProvider>
       </body>
     </html>
-  );
+  )
 }
