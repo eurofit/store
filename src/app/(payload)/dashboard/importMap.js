5b7abfe1ce364e31d269f51c58541b7e3cec7ecf
abdirazak <eurofitltd@gmail.com>
2026-01-10 18:20:03 +0300

    pump payload to 3.70

--- src/app/(payload)/dashboard/importMap.js
+++ src/app/(payload)/dashboard/importMap.js
@@ -3,11 +3,13 @@ import { ImageCell as ImageCell_7af2928281c4f36657f70d9a8be311cd } from '@/field
 import { CartTotalItemsCell as CartTotalItemsCell_23ccf96cb94accc854d864341654160c } from '@/fields/cart/total-items-cell'
 import { RowLabel as RowLabel_71423a18ca0efe5aeb5a62c885042753 } from '@/globals/nav/row-label'
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
+import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
 
 export const importMap = {
   "@/fields/slug/slug#Slug": Slug_65098561cd5bf6673f612173723e11fc,
   "@/fields/image-cell#ImageCell": ImageCell_7af2928281c4f36657f70d9a8be311cd,
   "@/fields/cart/total-items-cell#CartTotalItemsCell": CartTotalItemsCell_23ccf96cb94accc854d864341654160c,
   "@/globals/nav/row-label#RowLabel": RowLabel_71423a18ca0efe5aeb5a62c885042753,
-  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
+  "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
 }
