c69f62c89245fa98be592bca3a2a9f6fd8058aec
abdirazak <eurofitltd@gmail.com>
2026-01-17 21:29:59 +0300

    fix icon

--- src/components/payload-icon.tsx
+++ src/components/payload-icon.tsx
@@ -1,5 +1,5 @@
 import { HouseIcon } from 'lucide-react';
 
 export function PayloadIcon() {
-  return <HouseIcon className="text-muted-foreground size-4" />;
+  return <HouseIcon className="text-muted-foreground" size={16} />;
 }
