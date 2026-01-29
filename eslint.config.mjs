97511b219cd702e5580ecfeff3cd7df1c6dc5b3f
eurofit <eurofitltd@gmail.com>
2026-01-29 12:48:00 +0300

    fix lint crash

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,10 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
- 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
- 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
- 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+
+const config = [
+  ...nextCoreWebVitals,
   {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
+    ignores: ['node_modules/**'],
   },
-]
- 
-export default eslintConfig
+];
+
+export default config;
