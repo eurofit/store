0b1664c199d51dcd2e1bae7e054e66a0626f0db0
abdirazak <eurofitltd@gmail.com>
2026-02-14 17:47:50 +0300

    improve

--- src/utils/get-contact-us-json-ld.ts
+++ src/utils/get-contact-us-json-ld.ts
@@ -1,5 +1,5 @@
 import { site } from '@/constants/site';
-import { BreadcrumbList, WithContext } from 'schema-dts';
+import { BreadcrumbList, ContactPage, WebPage, WithContext } from 'schema-dts';
 
 export function getContactUsJsonLd() {
   const pageUrl = `${site.url}/contact-us`;
@@ -27,5 +27,31 @@ export function getContactUsJsonLd() {
     ],
   };
 
-  return [breadcrumb];
+  // ---------------- ContactPage ----------------
+  const contactPage: WithContext<ContactPage> = {
+    '@context': 'https://schema.org',
+    '@type': 'ContactPage',
+    '@id': `${pageUrl}#contactpage`,
+    url: pageUrl,
+    name: `Contact ${site.name}`,
+    isPartOf: { '@id': `${site.url}/#website` },
+    about: { '@id': `${site.url}/#organization` },
+    breadcrumb: { '@id': breadcrumbId },
+    mainEntity: { '@id': `${site.url}/#organization` },
+  };
+
+  // ---------------- WebPage ----------------
+  const webpage: WithContext<WebPage> = {
+    '@context': 'https://schema.org',
+    '@type': 'WebPage',
+    '@id': `${pageUrl}#webpage`,
+    url: pageUrl,
+    name: 'Contact Us',
+    isPartOf: { '@id': `${site.url}/#website` },
+    about: { '@id': `${site.url}/#organization` },
+    breadcrumb: { '@id': breadcrumbId },
+    mainEntity: { '@id': `${pageUrl}#contactpage` },
+  };
+
+  return [breadcrumb, contactPage, webpage];
 }
