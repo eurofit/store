b0e4bff0e8c829d1a228710cc0b4ee0c062d9f14
abdirazak <eurofitltd@gmail.com>
2026-03-08 23:21:15 +0300

    dev

--- src/app/(frontend)/(store)/step/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getCurrentUser } from '@/actions/auth/get-current-user';
-import { CheckoutStepperDemo } from '@/components/checkout/stepper';
-
-export default async function Page() {
-  const user = await getCurrentUser();
-  return <CheckoutStepperDemo user={user} />;
-}
