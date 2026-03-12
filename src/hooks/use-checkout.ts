6cbf67cee44dc8f7b2387c2bd626678ee3b3ddd0
abdirazak <eurofitltd@gmail.com>
2026-03-12 13:52:38 +0300

    use nextjs router to push chekcout page

--- src/hooks/use-checkout.ts
+++ src/hooks/use-checkout.ts
@@ -2,15 +2,17 @@
 
 import { checkout as checkoutAction } from '@/actions/checkout';
 import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 export function useCheckout() {
+  const router = useRouter();
+
   const checkoutMutation = useMutation({
     mutationFn: checkoutAction,
     mutationKey: ['checkout'],
     onSuccess: ({ data: { authorization_url } }) => {
-      // push to blank page
-      window.open(authorization_url, '_blank');
+      router.push(authorization_url);
     },
     onError: () => {
       toast.error('An unexpected error occurred.');
