2c1558b5a885e8f47330885010d9c1cff504f757
eurofit <eurofitltd@gmail.com>
2026-04-21 11:36:20 +0300

    fix

--- src/components/rich-text/converters/sm.tsx
+++ src/components/rich-text/converters/sm.tsx
@@ -17,7 +17,7 @@ export const smConverters: JSXConvertersFunction<NodeTypes> = ({
         : Array.isArray(content)
           ? content.join('')
           : '';
-    return <p className="text-sm leading-6 not-first:mt-2">{text}</p>;
+    return <p className="leading-6 not-first:mt-2">{text}</p>;
   },
   list: ({ node, nodesToJSX }) => {
     const content = nodesToJSX({ nodes: node.children });
@@ -26,7 +26,7 @@ export const smConverters: JSXConvertersFunction<NodeTypes> = ({
 
     return (
       <Comp
-        className={cn('my-3 ml-3 text-sm [&>li]:mt-2', {
+        className={cn('my-3 ml-3 [&>li]:mt-2', {
           'list-disc': node.listType === 'bullet',
           'list-decimal': node.listType === 'number',
           'list-inside [&>li]:before:content-["✓"]': node.listType === 'check',
