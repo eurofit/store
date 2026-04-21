7ea6c80e99aa66e74055df4d5f63a38b806f4782
eurofit <eurofitltd@gmail.com>
2026-04-21 13:12:07 +0300

    fix

--- src/components/rich-text/converters/p.tsx
+++ src/components/rich-text/converters/p.tsx
@@ -5,14 +5,6 @@ import { JSXConverters } from '@payloadcms/richtext-lexical/react';
 export const paragraphConverter: JSXConverters<SerializedParagraphNode> = {
   paragraph: ({ node, nodesToJSX }) => {
     const content = nodesToJSX({ nodes: node.children });
-
-    const text =
-      typeof content === 'string'
-        ? content
-        : Array.isArray(content)
-          ? content.join('')
-          : '';
-
-    return <P>{text}</P>;
+    return <P>{content}</P>;
   },
 };
