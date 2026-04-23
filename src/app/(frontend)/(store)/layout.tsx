b82e9844475bef8a3762fe090055d47807d9da67
eurofit <eurofitltd@gmail.com>
2026-04-24 00:27:53 +0300

    Update layout.tsx

--- src/app/(frontend)/(store)/layout.tsx
+++ src/app/(frontend)/(store)/layout.tsx
@@ -14,7 +14,7 @@ type StoreLayoutProps = Readonly<{
 export default async function StoreLayout({ children }: StoreLayoutProps) {
   return (
     <div className="container mx-auto">
-      <AnounceBar />
+      <AnnounceBar />
       <Header />
       <div className="relative min-h-[calc(100vh-5rem)] p-4 md:min-h-[calc(100vh-4rem)] md:p-6">
         {children}
